ab377e72660290330fc0689911421bfe389c8c9d
KGEW0511 <104777650+KGEW0511@users.noreply.github.com>
2023-08-29 20:18:01 +0900

    commit
    commit

---
+++ VR-Lecture/Assets/Tutorial/Scripts/Mob/Mob.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class Mob : MonoBehaviour
+{
+    public float destroyDelay = 1f;
+
+    public UnityEvent OnCreated;
+    public UnityEvent OnDestroyed;
+
+    private bool isDestroyed = false;
+
+    private void Start()
+    {
+        Invoke(nameof(Destroy), 3f);
+
+        OnCreated?.Invoke();
+    }
+    public void Destroy()
+    {
+        if (isDestroyed)
+            return;
+        isDestroyed = true;
+
+        Destroy(gameObject, destroyDelay);
+
+        OnDestroyed?.Invoke();
+    }
+}
