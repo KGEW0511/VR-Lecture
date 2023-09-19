3f9d080000100a6dcd52b6f7dd604e7cb5376168
KGEW0511 <104777650+KGEW0511@users.noreply.github.com>
2023-09-19 19:58:06 +0900

    어떻게 게임씬이랑 게임화면이 달라 무친

---
+++ VR-Lecture/Assets/Tutorial/Scripts/Effect/FlashVolume.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class FlashVolume : MonoBehaviour
+{
+    public float duration = 0.05f;
+
+    private Volume target;
+
+    private void Awake()
+    {
+        target = GetComponent<Volume>();
+    }
+
+    public void Call()
+    {
+        StopAllCoroutines();
+        StartCoroutine(Process());
+    }
+
+    private IEnumerator Process()
+    {
+        Debug.Log(target);
+        target.enabled = true;
+
+        yield return new WaitForSeconds(duration);
+
+        target.enabled = false;
+    }
+}
